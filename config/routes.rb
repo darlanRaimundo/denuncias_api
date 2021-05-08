86bacb84d29b97ace115fbfced5c55bfc135f902
DarlanRaimundo <darlanraimundo123@gmail.com>
2021-05-08 00:00:55 -0300

    Autenticação de usuario

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,9 @@
 require 'api_constraints'
 
 Rails.application.routes.draw do
-  mount_devise_token_auth_for 'User', at: 'auth'
   namespace :api, defaults: { format: :json } do
+    mount_devise_token_auth_for 'User', at: 'auth'
+    
     scope module: :v1,
           contraints: ApiConstraints.new(version: 1, default: true) do
       resources :denuncias
