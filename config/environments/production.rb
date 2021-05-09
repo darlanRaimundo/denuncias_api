9a935e37661856be82e14c348227aec9c4e1b9c8
DarlanRaimundo <darlanraimundo123@gmail.com>
2021-05-09 11:10:29 -0300

    Correção de problema no envio de email de confirmação

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -112,7 +112,7 @@ Rails.application.configure do
   # config.active_record.database_resolver_context = ActiveRecord::Middleware::DatabaseSelector::Resolver::Session
 
   # Email
-  config.action_mailer.default_url_options = { host: 'denuncias_api.herokuapp.com' }
+  config.action_mailer.default_url_options = { host: 'denuncias-api.herokuapp.com' }
   config.action_mailer.delivery_method = :smtp
   config.action_mailer.smtp_settings = {
     address: 'smtp.gmail.com',
